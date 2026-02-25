5a7745fafc624407e1b9ded1a6a552db327a0b85
Sara Almeida <sarafalmeida@ua.pt>
2026-02-25 17:07:11 +0000

    update

--- platform/backend.tf
+++ platform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "workshop-ua-sara-terraform-state-bucket"
+    bucket         = "workshop-ua-sara-dev-terraform-state"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
